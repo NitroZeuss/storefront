dce5888e77f35126dd24189b0503d4b401a5666d
Adwait <adwaitprime@gmail.com>
2023-09-25 23:02:56 +0530

    edited views and added seriliazers.py

--- store/views.py
+++ store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from .models import Product
+from .serializer import ProductSerializers
+from rest_framework.decorators import api_view
 
 
 # Create your views here.
 
+@api_view(['GET', 'POST'])
 def product_list(request):
-    return HttpResponse('initiated')
+    product = Product.objects.get(pk=id)
+    serializer = ProductSerializers(product)
+    return Response(serializer.data)
