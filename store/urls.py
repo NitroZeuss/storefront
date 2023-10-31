dcc223b99edcff624eeb147fc634642cadc3d2af
NitroZeuss <142437589+NitroZeuss@users.noreply.github.com>
2023-10-31 19:41:38 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,23 @@
+from decimal import Decimal
+from store.models import Product, Collection
+from rest_framework import serializers
 
-from django.urls import path
-from . import views
 
-urlpatterns = [
-    # Other URL patterns...
-    path('products/<int:id>/', views.product_detail),
-    path('products/', views.product_list)
-    # Other URL patterns...
-]
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
 
+    products_count = serializers.IntegerField()
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
